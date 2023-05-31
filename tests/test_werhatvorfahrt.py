dd681b64ceefbbd9fe3757a80617a4fd681c0b2d
Thomas <0milieux_member@icloud.com>
2023-05-31 09:57:28 +0200

    Initial commit after initializing the project.

---
+++ tests/test_werhatvorfahrt.py
@@ -0,0 +1,8 @@
+"""Tests for `werhatvorfahrt` package."""
+
+
+from werhatvorfahrt import __version__
+
+
+def test_version() -> None:
+    assert __version__ == "0.1.0"
---
+++ werhatvorfahrt/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for werhatvorfahrt."""
+from werhatvorfahrt.utils.config import config
+from werhatvorfahrt.utils.logger import logger
+
+__all__ = ["config", "logger"]
+__author__ = """Thomas Camminady"""
+__email__ = "0milieux_member@icloud.com"
+__version__ = "0.1.0"
